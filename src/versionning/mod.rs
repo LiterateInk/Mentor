55ebe14f4308293b9894b6c52b6d9806e48bff48
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-12-26 16:28:21 +0100

    feat: add versionning utility

---
+++ src/versionning/mod.rs
@@ -0,0 +1,44 @@
+use cargo_toml::Manifest;
+use std::{env::current_dir, io::Error};
+
+pub mod kotlin;
+pub mod swift;
+pub mod rust;
+
+pub fn get_current_version () -> Result<Vec<u8>, cargo_toml::Error> {
+  let path = current_dir()?.join("Cargo.toml");
+  let manifest = Manifest::from_path(path)?;
+  let version = manifest.package().version();
+
+  Ok(string_to_version(version))
+}
+
+pub fn bump_version (version: &[u8], index: usize) -> Vec<u8> {
+  let mut version = version.to_vec();
+
+  match index {
+    // major
+    0 => {
+      version[0] += 1;
+      version[1..].fill(0);
+    },
+    // minor
+    1 => {
+      version[1] += 1;
+      version[2..].fill(0);
+    },
+    // patch
+    2 => version[2] += 1,
+    _ => unreachable!(),
+  }
+
+  version
+}
+
+pub fn version_to_string (version: Vec<u8>) -> String {
+  version.iter().map(|part| part.to_string()).collect::<Vec<String>>().join(".")
+}
+
+pub fn string_to_version (version: &str) -> Vec<u8> {
+  version.split(".").map(|part| part.parse().unwrap()).collect()
+}
