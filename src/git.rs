c7b308384f7610a8885386e2d808d2c20a39f28e
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-12-27 04:25:09 +0100

    feat: configure git for github action

---
+++ src/git.rs
@@ -0,0 +1,22 @@
+use std::process::{Command, Output};
+use colored::Colorize;
+use anyhow::{Ok, Result};
+
+pub fn run (args: &[&str]) -> Result<Output> {
+  let log = format!("+> git {}", args.join(" "));
+  println!("{}", log.bright_black());
+
+  let output = Command::new("git")
+    .args(args)
+    .output()?;
+
+  Ok(output)
+}
+
+/// sets the user to github actions bot.
+pub fn configure () -> Result<()> {
+  run(&["config", "user.name", "github-actions[bot]"])?;
+  run(&["config", "user.email", "github-actions[bot]@users.noreply.github.com"])?;
+
+  Ok(())
+}
