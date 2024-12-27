ee2541d070c01e399aacac20bc58a7f95d822a8e
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-12-27 04:25:24 +0100

    feat(swift): apply new checksum to `Package.swift`

--- src/versionning/swift.rs
+++ src/versionning/swift.rs
@@ -41,3 +41,26 @@ pub fn apply_version (old_version: &str, new_version: &str) -> anyhow::Result<()
     Ok(())
   }
 }
+
+pub fn apply_checksum (new_checksum: &str) -> anyhow::Result<()> {
+  let old_content = io::read_file_as_string(package_swift_path()?)?;
+
+  let checksum_regex = Regex::new(r#"checksum: "(.*)"\),"#)?;
+  let captures = checksum_regex.captures(&old_content)
+    .expect("checksum not found");
+
+  let old_checksum = captures.get(1)
+    .expect("checksum not captured")
+    .as_str();
+
+  // debug: show the difference
+  println!("{}", format!("-checksum: \"{}\"", old_checksum).red());
+  println!("{}", format!("+checksum: \"{}\"", new_checksum).green());
+
+  let new_content = old_content.replace(old_checksum, new_checksum);
+
+  let mut file = File::create(package_swift_path()?)?;
+  file.write_all(new_content.as_bytes())?;
+
+  Ok(())
+}
