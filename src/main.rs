d690f4a91f6146803ecb1310250e72d777286452
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-12-31 03:20:56 +0100

    chore(js): make the cli a bit nicer

--- src/main.rs
+++ src/main.rs
@@ -313,46 +313,50 @@ fn main() -> anyhow::Result<()> {
       let types_file_path = current_dir()?.join("target/wasm-js-staging/index.d.ts");
       let types = io::read_file_as_string(types_file_path)?;
 
-      println!("applying optional and default 'fetcher' to bindings...");
-      let js = format!("{}{}", "const{defaultFetcher}=require(\"@literate.ink/utilities/fetcher\");", js)
-        .replace(", fetcher) {", ",fetcher=defaultFetcher){");
+      println!("\napplying defaultFetcher to JS bindings...");
+      println!("=> adding CJS import for defaultFetcher");
+      let js = format!("{}{}", "const { defaultFetcher } = require(\"@literate.ink/utilities/fetcher\");", js);
+      println!("=> rewriting every instance of fetcher to make it use defaultFetcher by default");
+      let js = js.replace(", fetcher) {", ", fetcher = defaultFetcher) {");
 
-      println!("removing init exports...");
+      println!("\ncleaning up js bindings...");
+      println!("=> removing {} and {} exports", "initSync".bold(), "__wbg_init".bold());
       let js = js
         .replace("export { initSync };", "")
         .replace("export default __wbg_init;", "");
 
-      println!("removing '__wbg_init' function...");
+      println!("=> removing {} function", "__wbg_init".bold());
       let js = strings::remove_from_until(&js,
         "async function __wbg_init",
         "return __wbg_finalize_init(instance, module);\n}", true
       );
 
+      println!("=> removing {} old property", "__wbg_init.__wbindgen_wasm_module".bold());
       let js = js.replace("__wbg_init.__wbindgen_wasm_module = module;", "");
 
-      println!("removing useless parts in 'initSync' function...");
+      println!("=> removing dead parts in {} function", "initSync".bold());
       let js = strings::remove_from_until(&js,
         "if (typeof module !== 'undefined') {",
         "const imports = __wbg_get_imports();", false
       );
-
       let js = strings::remove_from_until(&js,
         "if (!(module instanceof WebAssembly.Module)) {",
         "}\n", true
       );
 
+      println!("=> rewriting module instanciation in {} function", "initSync".bold());
       let js = js.replace(
         "new WebAssembly.Instance(module",
         "new WebAssembly.Instance(new WebAssembly.Module(module)"
       );
 
-      println!("extracting and removing exports...");
+      println!("\nextracting and removing exports...");
       let mut exports: Vec<String> = Vec::new();
 
       let class_re = Regex::new(r"export class (\w+)").unwrap();
       let js = class_re.replace_all(&js, |caps: &Captures| {
           let class_name = &caps[1];
-          println!("found class: {}", class_name);
+          println!("=> found class: {}", class_name.bold());
           exports.push(class_name.to_string());
           format!("class {}", class_name)
       }).to_string();
@@ -360,7 +364,7 @@ fn main() -> anyhow::Result<()> {
       let function_re = Regex::new(r"export function (\w+)").unwrap();
       let js = function_re.replace_all(&js, |caps: &Captures| {
           let function_name = &caps[1];
-          println!("found function: {}", function_name);
+          println!("=> found function: {}", function_name.bold());
           exports.push(function_name.to_string());
           format!("function {}", function_name)
       }).to_string();
@@ -368,12 +372,12 @@ fn main() -> anyhow::Result<()> {
       let const_re = Regex::new(r"export const (\w+)").unwrap();
       let js = const_re.replace_all(&js, |caps: &Captures| {
           let const_name = &caps[1];
-          println!("found constant: {}", const_name);
+          println!("=> found constant: {}", const_name.bold());
           exports.push(const_name.to_string());
           format!("const {}", const_name)
       }).to_string();
 
-      println!("embeding wasm as base64...");
+      println!("\nembeding wasm as base64...");
       let wasm_file_path = current_dir()?.join("target/wasm-js-staging/index_bg.wasm");
       let wasm = io::read_file_as_base64url(wasm_file_path)?;
       let mut js = js;
@@ -384,42 +388,70 @@ fn main() -> anyhow::Result<()> {
         void initSync(stringToBytes("base64url", _code));
       "#, "const { stringToBytes } = require(\"@scure/base\");").as_ref();
 
-      println!("rewriting exports...");
+      println!("=> wasm base64url length: {}", wasm.len().to_string().yellow());
+
+      println!("\nrewriting exports...");
       for export in exports {
+        println!("=> exporting: {}", export.bold());
         js += format!("exports.{export}={export};\n").as_ref();
       }
 
       // add the type import at the top
-      println!("applying optional and correct type 'fetcher'");
-      let types = format!("{}\n\n{}", "import type { Fetcher } from \"@literate.ink/utilities/fetcher\";", types)
-        // replace every function signature with an optional fetcher (instead of a required one with no type)
-        .replace("fetcher: Function)", "fetcher?: Fetcher)");
+      println!("\napplying optional and correct type 'fetcher'...");
+      let types = format!("{}\n\n{}", "import type { Fetcher } from \"@literate.ink/utilities/fetcher\";", types);
 
-      println!("removing useless types exports...");
+      // replacing types for `fetcher` argument
+      let from = "fetcher: Function)";
+      let to = "fetcher?: Fetcher)";
+      let types = types.replace("fetcher: Function)", "fetcher?: Fetcher)");
+      println!("=> replacing '{}' with '{}'", from.red(), to.green());
+
+      println!("\ncleaning up types...");
+      println!("=> removing useless exports");
       let types= strings::remove_from_until(&types,
         "export type InitInput",
         "Promise<InitOutput>;", true
       );
 
-      println!("correct optional paramaters types (`arg: T | null | undefined` into `arg?: T | null`");
-      let regex = Regex::new(r"(\w+): ((.*) \| undefined)").unwrap();
+      println!("\ncorrecting optional parameters types...");
+
+      // Has been tested against these cases :
+      //
+      // nip: string | null | undefined
+      // nip: Uint8Array | ArrayBuffer | null | undefined
+      // (session: Session | null | undefined, nip: string | null | undefined, fetcher?: Fetcher): Promise<Uint8Array>;
+      // (session: Session, nip: Uint8Array | ArrayBuffer | null | undefined, fetcher?: Fetcher): Promise<Uint8Array>;
+      // (session: Session, nip: Uint8Array | ArrayBuffer | null | undefined, fetcher?: Fetcher): Promise<Uint8Array> | null | undefined;
+      // () => string | null | undefined;
+      // type Session = {
+      //   url: string | null | undefined
+      //   url: string | null | undefined;url: string | null | undefined
+      //   name: SessionName | SessionNaming | null | undefined,
+      //   name: SessionName | SessionNaming | null | undefined
+      //   age: number
+      // }
+      let regex = Regex::new(r"(\b\w+\b): ([^,;\n]*) \| undefined").unwrap();
       let types = regex.replace_all(&types, |caps: &Captures| {
-        format!("{}?: {}", &caps[1], &caps[3])
+        let replacement = format!("{}?: {}", &caps[1], &caps[2]);
+        println!("=> replacing '{}' with '{}'", &caps[0].red(), &replacement.green());
+        replacement
       }).to_string();
 
       // cleanup
       let _ = std::fs::remove_dir_all(current_dir()?.join("js"));
       create_dir_all("js")?;
 
-      println!("outputting types...");
+      println!("\noutputting types...");
       let types_file_path = current_dir()?.join("js/index.d.ts");
-      io::write_string_to_file(types_file_path, types)?;
+      io::write_string_to_file(types_file_path.clone(), types)?;
+      println!("=> types file path: {}", types_file_path.to_str().unwrap().underline());
 
-      println!("outputting bindings...");
+      println!("\noutputting bindings...");
       let js_file_path = current_dir()?.join("js/index.js");
-      io::write_string_to_file(js_file_path, js)?;
+      io::write_string_to_file(js_file_path.clone(), js)?;
+      println!("=> js file path: {}", js_file_path.to_str().unwrap().underline());
 
-      println!("minifying bindings...");
+      println!("\nminifying bindings...");
       let mut child = Command::new("terser")
         .args([
           "js/index.js",
@@ -430,6 +462,8 @@ fn main() -> anyhow::Result<()> {
         .spawn()?;
 
       child.wait()?;
+
+      println!("\ndone ! you can now publish the package.");
     },
     Commands::Version => {
       let version = version_to_string(get_current_version()?);
